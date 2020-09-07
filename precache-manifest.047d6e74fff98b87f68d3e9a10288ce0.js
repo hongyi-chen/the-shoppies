80a47147bf476b0ff994beaae35e19d8b2a287b7
Hong Yi Chen <hongyigma@gmail.com>
2020-09-07 01:18:42 -0400

    Updates

---
+++ precache-manifest.047d6e74fff98b87f68d3e9a10288ce0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d7104e098fa46e2768fd523ab3ebd8",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "b37e2e8097fa7b096b6d",
+    "url": "/the-shoppies/static/css/main.d2a1426f.chunk.css"
+  },
+  {
+    "revision": "9cdf270facc90ddbab39",
+    "url": "/the-shoppies/static/js/2.a59f94af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-shoppies/static/js/2.a59f94af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37e2e8097fa7b096b6d",
+    "url": "/the-shoppies/static/js/main.d5d43d70.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/the-shoppies/static/media/close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "0e2e1f67a9f6a790c1dfcdbc28d63e5b",
+    "url": "/the-shoppies/static/media/search.0e2e1f67.png"
+  },
+  {
+    "revision": "1dd099a78bcac4eb39749d0df6680c93",
+    "url": "/the-shoppies/static/media/shopping-bag.1dd099a7.png"
+  }
+]);
